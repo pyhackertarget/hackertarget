53232a064cc7863e55146464a6969e8ffc4daa08
İsmail Taşdelen <ismailtasdelen@users.noreply.github.com>
2019-08-06 10:38:57 +0300

    resolves issue update
    + travis cli
    + test app
    + setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+
+setup(name='hackertarget',
+      version='1.4',
+      description='HackerTarget',
+      url='https://github.com/ismailtasdelen/hackertarget',
+      author='İSMAİL TAŞDELEN',
+      author_email='pentestdatabase@gmail.com',
+      license='MIT',
+      packages=['source'],
+      python_requires=">=3",
+      zip_safe=False)
