3ee09ca281bea175c03092ba24a3ddcfd71826ba
İsmail Taşdelen <ismailtasdelen@users.noreply.github.com>
2019-08-12 00:36:29 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='hackertarget',
-      version='0.1',
+      version='2.0',
       description='Hacker Target',
       url='https://github.com/ismailtasdelen/hackertarget',
       author='İSMAİL TAŞDELEN',
