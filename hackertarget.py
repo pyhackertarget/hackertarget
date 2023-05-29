8c648f8272ae388151cdcdcdeaac51f8ac8db988
ckingn <124626630+ckingn@users.noreply.github.com>
2023-05-29 07:45:12 -0600

    Update hackertarget.py
    Blocked team-: script> if ( location. Hash slice (1) documents get element by I'd ('"al") style- csstext- color:; location hash slice (1)<{script) www. Victims .com /\#red:-0- link< JavaScript:-alert(1)~-0-link-aource: current: (opera(8,12) www.victim.com/\#red-expression(alert(url=1)),(1E7181)

--- hackertarget.py
+++ hackertarget.py
@@ -1,5 +1,842 @@
 #!/usr/bin/env python
-# -*- coding: utf-8 -*-
+Toggle navigation
+Toggle navigation
+SuperTool Beta7
+157.7.231.116
+blacklist:157.7.231.116   
+Blacklist advertisement image
+Checking 157.7.231.116 against 82 known blacklists...
+Listed 0 times with 2 timeouts
+
+Blacklist
+Reason
+TTL
+ResponseTime
+Status Ok OK
+0SPAM
+47
+Status Ok OK
+Abuse.ro
+108
+Status Ok OK
+Abusix Mail Intelligence Blacklist
+0
+Status Ok OK
+Abusix Mail Intelligence Domain Blacklist
+0
+Status Ok OK
+Abusix Mail Intelligence Exploit list
+0
+Status Ok OK
+Anonmails DNSBL
+108
+Status Ok OK
+BACKSCATTERER
+0
+Status Ok OK
+BARRACUDA
+17
+Status Ok OK
+BLOCKLIST.DE
+0
+Status Ok OK
+CALIVENT
+17
+Status Ok OK
+CYMRU BOGONS
+0
+Status Ok OK
+DAN TOR
+268
+Status Ok OK
+DAN TOREXIT
+258
+Status Ok OK
+DNS SERVICIOS
+108
+Status Ok OK
+DRMX
+211
+Status Ok OK
+DRONE BL
+27
+Status Ok OK
+FABELSOURCES
+17
+Status Ok OK
+HIL
+16
+Status Ok OK
+HIL2
+16
+Status Ok OK
+Hostkarma Black
+75
+Status Ok OK
+IBM DNS Blacklist
+17
+Status Ok OK
+ICMFORBIDDEN
+124
+Status Ok OK
+IMP SPAM
+16
+Status Ok OK
+IMP WORM
+16
+Status Ok OK
+INTERSERVER
+17
+Status Ok OK
+ivmSIP
+16
+Status Ok OK
+ivmSIP24
+17
+Status Ok OK
+JIPPG
+173
+Status Ok OK
+KEMPTBL
+17
+Status Ok OK
+KISA
+211
+Status Ok OK
+Konstant
+17
+Status Ok OK
+LASHBACK
+17
+Status Ok OK
+LNSGBLOCK
+11
+Status Ok OK
+LNSGBULK
+10
+Status Ok OK
+LNSGMULTI
+10
+Status Ok OK
+LNSGOR
+10
+Status Ok OK
+LNSGSRC
+10
+Status Ok OK
+MADAVI
+108
+Status Ok OK
+MAILSPIKE BL
+59
+Status Ok OK
+MAILSPIKE Z
+59
+Status Ok OK
+MSRBL Phishing
+10
+Status Ok OK
+MSRBL Spam
+91
+Status Ok OK
+NETHERRELAYS
+31
+Status Ok OK
+NETHERUNSURE
+31
+Status Ok OK
+NIXSPAM
+0
+Status Ok OK
+Nordspam BL
+0
+Status Ok OK
+ORVEDB
+144
+Status Ok OK
+PSBL
+0
+Status Ok OK
+RATS Dyna
+0
+Status Ok OK
+RATS NoPtr
+0
+Status Ok OK
+RATS Spam
+0
+Status Ok OK
+RBL JP
+0
+Status Ok OK
+SCHULTE
+20
+Status Ok OK
+SEM BACKSCATTER
+20
+Status Ok OK
+SEM BLACK
+20
+Status Ok OK
+Sender Score Reputation Network
+0
+Status Ok OK
+SERVICESNET
+20
+Status Ok OK
+SORBS BLOCK
+0
+Status Ok OK
+SORBS DUHL
+0
+Status Ok OK
+SORBS HTTP
+0
+Status Ok OK
+SORBS MISC
+0
+Status Ok OK
+SORBS NEW
+0
+Status Ok OK
+SORBS SMTP
+0
+Status Ok OK
+SORBS SOCKS
+10
+Status Ok OK
+SORBS SPAM
+10
+Status Ok OK
+SORBS WEB
+10
+Status Ok OK
+SORBS ZOMBIE
+10
+Status Ok OK
+SPAMCOP
+10
+Status Ok OK
+Spamhaus ZEN
+10
+Status Ok OK
+SPFBL DNSBL
+114
+Status Ok OK
+Suomispam Reputation
+81
+Status Ok OK
+SWINOG
+0
+Status Ok OK
+TRIUMF
+0
+Status Ok OK
+TRUNCATE
+10
+Status Ok OK
+UCEPROTECTL1
+0
+Status Ok OK
+UCEPROTECTL2
+0
+Status Ok OK
+UCEPROTECTL3
+0
+Status Ok OK
+Woodys SMTP Blacklist
+0
+Status Ok OK
+WPBL
+0
+Status Ok OK
+ZapBL
+81
+Status Warning TIMEOUT
+NoSolicitado
+0
+Ignore
+Status Warning TIMEOUT
+RSBL
+0
+Ignore
+reverse lookup	smtp diag	subnet tool
+Reported by mxtoolbox.com on 5/29/2023 at 2:16:39 PM, just for you.  Transcript
+blacklist:157.7.231.116   
+Checking 157.7.231.116 against 82 known blacklists...
+Listed 0 times with 1 timeouts
+
+Blacklist
+Reason
+TTL
+ResponseTime
+Status Ok OK
+0SPAM
+131
+Status Ok OK
+Abuse.ro
+131
+Status Ok OK
+Abusix Mail Intelligence Blacklist
+0
+Status Ok OK
+Abusix Mail Intelligence Domain Blacklist
+0
+Status Ok OK
+Abusix Mail Intelligence Exploit list
+0
+Status Ok OK
+Anonmails DNSBL
+131
+Status Ok OK
+BACKSCATTERER
+0
+Status Ok OK
+BARRACUDA
+10
+Status Ok OK
+BLOCKLIST.DE
+0
+Status Ok OK
+CALIVENT
+10
+Status Ok OK
+CYMRU BOGONS
+10
+Status Ok OK
+DAN TOR
+269
+Status Ok OK
+DAN TOREXIT
+269
+Status Ok OK
+DNS SERVICIOS
+131
+Status Ok OK
+DRMX
+204
+Status Ok OK
+DRONE BL
+131
+Status Ok OK
+FABELSOURCES
+11
+Status Ok OK
+HIL
+0
+Status Ok OK
+HIL2
+0
+Status Ok OK
+Hostkarma Black
+121
+Status Ok OK
+IBM DNS Blacklist
+0
+Status Ok OK
+ICMFORBIDDEN
+121
+Status Ok OK
+IMP SPAM
+0
+Status Ok OK
+IMP WORM
+0
+Status Ok OK
+INTERSERVER
+121
+Status Ok OK
+ivmSIP
+0
+Status Ok OK
+ivmSIP24
+0
+Status Ok OK
+JIPPG
+143
+Status Ok OK
+KEMPTBL
+121
+Status Ok OK
+KISA
+206
+Status Ok OK
+Konstant
+121
+Status Ok OK
+LASHBACK
+0
+Status Ok OK
+LNSGBLOCK
+121
+Status Ok OK
+LNSGBULK
+121
+Status Ok OK
+LNSGMULTI
+121
+Status Ok OK
+LNSGOR
+121
+Status Ok OK
+LNSGSRC
+121
+Status Ok OK
+MADAVI
+121
+Status Ok OK
+MAILSPIKE BL
+121
+Status Ok OK
+MAILSPIKE Z
+121
+Status Ok OK
+MSRBL Phishing
+121
+Status Ok OK
+MSRBL Spam
+121
+Status Ok OK
+NETHERRELAYS
+121
+Status Ok OK
+NETHERUNSURE
+121
+Status Ok OK
+NIXSPAM
+121
+Status Ok OK
+Nordspam BL
+121
+Status Ok OK
+ORVEDB
+143
+Status Ok OK
+PSBL
+121
+Status Ok OK
+RATS Dyna
+121
+Status Ok OK
+RATS NoPtr
+111
+Status Ok OK
+RATS Spam
+110
+Status Ok OK
+RBL JP
+110
+Status Ok OK
+RSBL
+142
+Status Ok OK
+SCHULTE
+110
+Status Ok OK
+SEM BACKSCATTER
+153
+Status Ok OK
+SEM BLACK
+43
+Status Ok OK
+Sender Score Reputation Network
+10
+Status Ok OK
+SERVICESNET
+43
+Status Ok OK
+SORBS BLOCK
+10
+Status Ok OK
+SORBS DUHL
+10
+Status Ok OK
+SORBS HTTP
+10
+Status Ok OK
+SORBS MISC
+10
+Status Ok OK
+SORBS NEW
+0
+Status Ok OK
+SORBS SMTP
+0
+Status Ok OK
+SORBS SOCKS
+0
+Status Ok OK
+SORBS SPAM
+0
+Status Ok OK
+SORBS WEB
+0
+Status Ok OK
+SORBS ZOMBIE
+0
+Status Ok OK
+SPAMCOP
+0
+Status Ok OK
+Spamhaus ZEN
+0
+Status Ok OK
+SPFBL DNSBL
+123
+Status Ok OK
+Suomispam Reputation
+85
+Status Ok OK
+SWINOG
+0
+Status Ok OK
+TRIUMF
+10
+Status Ok OK
+TRUNCATE
+22
+Status Ok OK
+UCEPROTECTL1
+10
+Status Ok OK
+UCEPROTECTL2
+10
+Status Ok OK
+UCEPROTECTL3
+10
+Status Ok OK
+Woodys SMTP Blacklist
+10
+Status Ok OK
+WPBL
+10
+Status Ok OK
+ZapBL
+85
+Status Warning TIMEOUT
+NoSolicitado
+0
+Ignore
+reverse lookup	smtp diag	subnet tool
+Reported by mxtoolbox.com on 5/29/2023 at 2:16:39 PM, just for you.  Transcript
+blacklist:157.7.231.116   
+Checking 157.7.231.116 against 82 known blacklists...
+Listed 0 times with 2 timeouts
+
+Blacklist
+Reason
+TTL
+ResponseTime
+Status Ok OK
+0SPAM
+11
+Status Ok OK
+Abuse.ro
+124
+Status Ok OK
+Abusix Mail Intelligence Blacklist
+1
+Status Ok OK
+Abusix Mail Intelligence Domain Blacklist
+1
+Status Ok OK
+Abusix Mail Intelligence Exploit list
+0
+Status Ok OK
+Anonmails DNSBL
+0
+Status Ok OK
+BACKSCATTERER
+0
+Status Ok OK
+BARRACUDA
+0
+Status Ok OK
+BLOCKLIST.DE
+0
+Status Ok OK
+CALIVENT
+0
+Status Ok OK
+CYMRU BOGONS
+0
+Status Ok OK
+DAN TOR
+0
+Status Ok OK
+DAN TOREXIT
+0
+Status Ok OK
+DNS SERVICIOS
+113
+Status Ok OK
+DRMX
+0
+Status Ok OK
+DRONE BL
+0
+Status Ok OK
+FABELSOURCES
+0
+Status Ok OK
+HIL
+0
+Status Ok OK
+HIL2
+0
+Status Ok OK
+Hostkarma Black
+0
+Status Ok OK
+IBM DNS Blacklist
+0
+Status Ok OK
+ICMFORBIDDEN
+0
+Status Ok OK
+IMP SPAM
+0
+Status Ok OK
+IMP WORM
+0
+Status Ok OK
+INTERSERVER
+10
+Status Ok OK
+ivmSIP
+0
+Status Ok OK
+ivmSIP24
+0
+Status Ok OK
+JIPPG
+10
+Status Ok OK
+KEMPTBL
+10
+Status Ok OK
+Konstant
+10
+Status Ok OK
+LASHBACK
+10
+Status Ok OK
+LNSGBLOCK
+10
+Status Ok OK
+LNSGBULK
+10
+Status Ok OK
+LNSGMULTI
+10
+Status Ok OK
+LNSGOR
+10
+Status Ok OK
+LNSGSRC
+10
+Status Ok OK
+MADAVI
+0
+Status Ok OK
+MAILSPIKE BL
+62
+Status Ok OK
+MAILSPIKE Z
+72
+Status Ok OK
+MSRBL Phishing
+0
+Status Ok OK
+MSRBL Spam
+0
+Status Ok OK
+NETHERRELAYS
+0
+Status Ok OK
+NETHERUNSURE
+0
+Status Ok OK
+NIXSPAM
+0
+Status Ok OK
+Nordspam BL
+0
+Status Ok OK
+ORVEDB
+0
+Status Ok OK
+PSBL
+0
+Status Ok OK
+RATS Dyna
+0
+Status Ok OK
+RATS NoPtr
+0
+Status Ok OK
+RATS Spam
+0
+Status Ok OK
+RBL JP
+0
+Status Ok OK
+RSBL
+0
+Status Ok OK
+SCHULTE
+0
+Status Ok OK
+SEM BACKSCATTER
+0
+Status Ok OK
+SEM BLACK
+0
+Status Ok OK
+Sender Score Reputation Network
+0
+Status Ok OK
+SERVICESNET
+10
+Status Ok OK
+SORBS BLOCK
+0
+Status Ok OK
+SORBS DUHL
+0
+Status Ok OK
+SORBS HTTP
+10
+Status Ok OK
+SORBS MISC
+10
+Status Ok OK
+SORBS NEW
+10
+Status Ok OK
+SORBS SMTP
+10
+Status Ok OK
+SORBS SOCKS
+11
+Status Ok OK
+SORBS SPAM
+10
+Status Ok OK
+SORBS WEB
+11
+Status Ok OK
+SORBS ZOMBIE
+11
+Status Ok OK
+SPAMCOP
+11
+Status Ok OK
+Spamhaus ZEN
+11
+Status Ok OK
+SPFBL DNSBL
+1
+Status Ok OK
+Suomispam Reputation
+0
+Status Ok OK
+SWINOG
+0
+Status Ok OK
+TRIUMF
+10
+Status Ok OK
+TRUNCATE
+10
+Status Ok OK
+UCEPROTECTL1
+0
+Status Ok OK
+UCEPROTECTL2
+0
+Status Ok OK
+UCEPROTECTL3
+0
+Status Ok OK
+Woodys SMTP Blacklist
+0
+Status Ok OK
+WPBL
+0
+Status Ok OK
+ZapBL
+10
+Status Warning TIMEOUT
+KISA
+0
+Ignore
+Status Warning TIMEOUT
+NoSolicitado
+0
+Ignore
+reverse lookup	smtp diag	subnet tool
+Reported by mxtoolbox.com on 5/29/2023 at 2:16:39 PM, just for you.  Transcript
+mx:macos.com     
+Mx advertisement image
+Pref
+Hostname
+IP Address
+TTL
+10
+mx13.gmoserver.jp
+157.7.231.116
+INTERQ (AS7506)
+10 min
+Blacklist Check      SMTP Test
+Test
+Result
+Status Problem
+DMARC Record Published
+No DMARC Record found
+Information More Info
+Status Warning
+DMARC Policy Not Enabled
+DMARC Quarantine/Reject policy not enabled
+Information More Info
+Status Ok
+DNS Record Published
+DNS Record found
+dns lookup	dns check	dmarc lookup	spf lookup	dns propagation
+Reported by dns01.gmoserver.jp on 5/29/2023 at 2:15:26 PM (UTC -5), just for you.  Transcript
+ABOUT THE SUPERTOOL!
+All of your MX record, DNS, blacklist and SMTP diagnostics in one integrated tool.  Input a domain name or IP Address or Host Name. Links in the results will guide you to other relevant tools and information.  And you'll have a chronological history of your results. 
+
+If you already know exactly what you want, you can force a particular test or lookup.  Try some of these examples:
+
+(e.g. "blacklist: 127.0.0.2" will do a blacklist lookup)
+
+Command	 	Explanation
+blacklist:	 	Check IP or host for reputation
+smtp:	 	Test mail server SMTP (port 25)
+mx:	 	DNS MX records for domain
+a:	 	DNS A record IP address for host name
+spf:	 	Check SPF records on a domain
+txt:	 	Check TXT records on a domain
+ptr:	 	DNS PTR record for host name
+cname:	 	DNS canonical host name to IP address
+whois:	 	Get domain registration information
+arin:		Get IP address block information
+soa:		Get Start of Authority record for a domain
+tcp:		Verify an IP Address allows tcp connections
+http:		Verify a URL allows http connections  
+https:		Verify a URL allows secure http connections  
+ping:		Perform a standard ICMP ping
+trace:		Perform a standard ICMP trace route
+dns:		Check your DNS Servers for possible problems  New!
+ 	 	 
+Other tools
+
+Feedback: If you run into any problems with the site or have an idea that you think would make it better, we would appreciate your feedback. Please leave us some Feedback.
+
+Your IP is: 15.204.9.28|  Contact Terms & Conditions Site Map API Privacy Phone: (866)-MXTOOLBOX / (866)-698-6652 |  © Copyright 2004-2021, MXToolBox, Inc, All rights reserved. US Patents 10839353 B2 & 11461738 B2
+ 
+burritos@banana-pancakes.com braunstrowman@banana-pancakes.com finnbalor@banana-pancakes.com ricflair@banana-pancakes.com randysavage@banana-pancakes.com -*- coding: utf-8 -*-
 
 from source import hackertarget_api
 import time
